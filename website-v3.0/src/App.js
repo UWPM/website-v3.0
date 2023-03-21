2e0b6bafb5724e3de2cd6310c04863e95feefc1a
Annie Cai <caishajia@gmail.com>
2023-03-20 22:13:46 -0400

    added routing to navbar

--- website-v3.0/src/App.js
+++ website-v3.0/src/App.js
@@ -1,26 +1,31 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './pages/HomePage'
-
 import React from "react";
 import { useState } from "react";
+import { Routes, Route, Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import logo from "./images/logos/pmlogo.svg";
 
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import Community from './pages/Community'
+import PMGuide from './pages/PMGuide'
+import PMJobs from './pages/PMJobs'
+import Contact from './pages/Contact'
 
 function App() {
+
   return (
     <div className={`App w-100`}>
 
       {/* Default bootstrap react navbar */}
       <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><img
+        <Navbar.Brand as={Link} to="/"><img
                   src={logo}
                   className="logo"
                   alt="University of Waterloo Product Management Club Logo"
@@ -28,19 +33,28 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto nav-right">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#community">Community</Nav.Link>
-            <Nav.Link href="#pm-guide">PM Guide</Nav.Link>
-            <Nav.Link href="#pm-jobs">PM Jobs</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/community">Community</Nav.Link>
+            <Nav.Link as={Link} to="/pm-guide">PM Guide</Nav.Link>
+            <Nav.Link as={Link} to="/pm-jobs">PM Jobs</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
 
-      
       <div style={{height: "100vh", }}>
-        <HomePage/>
+      <HomePage/>
+      </div>
+      <div>
+      <Routes>
+        <Route exact path="/" element={App} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/community" element={<Community/>} />
+        <Route exact path="/pm-guide" element={<PMGuide/>} />
+        <Route exact path="/pm-jobs" element={<PMJobs/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+      </Routes>
       </div>
     </div>
   );
