c3b10ef5fbb4a1d1a85a3e12ac6a0bf8c9cae4e3
Ahmed Ahmed <ahmedhmagzoub@gmail.com>
2023-06-19 19:30:12 -0600

    Added about us and get involved buttons

--- website-v3.0/src/components/Introduction.js
+++ website-v3.0/src/components/Introduction.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import "../styles/HomePage.css";
-import "../App.css"
+import "../App.css";
+import { Link } from 'react-router-dom';
 
 export default function Introduction({ show }) {
   return (
     <>
       <h2 className="mt-5">Welcome</h2>
       <div style={{minHeight: "200px"}}>Insert Code Here</div>
+      <div>
+        <div class="button-container">
+          <Link to="/about">
+            <button class='button-red inroduction-about-us'>
+              ABOUT US
+            </button>
+          </Link>
+          <Link to="/community">
+            <button class='button-transparent'>
+              GET INVOLVED
+            </button> 
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
