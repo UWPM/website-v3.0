723f6fba652ed9ef0a89238eef8b712d7a715418
Annie Cai <caishajia@gmail.com>
2023-06-28 10:43:38 -0400

    fixed red button styling on past events

--- website-v3.0/src/components/PastEvents.js
+++ website-v3.0/src/components/PastEvents.js
@@ -64,7 +64,7 @@ export default function PastEvents({ show }) {
       </Carousel>
       <div className="d-flex justify-content-center my-4">
         <Link to="/about">
-          <button class='button-red'>
+          <button class='button-red-gradiant'>
             VIEW ALL EVENTS
           </button>
         </Link>
