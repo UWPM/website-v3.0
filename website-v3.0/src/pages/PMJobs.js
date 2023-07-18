e17bfd9cbe9a66d932a81a86ba232b59deb35093
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2023-07-17 23:11:46 -0400

    Create table

--- website-v3.0/src/pages/PMJobs.js
+++ website-v3.0/src/pages/PMJobs.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import JobsTable from "../components/JobsTable";
 
 export default function PMJobs({ show }) {
-
   return (
     <>
       <div>This is the PM Jobs page</div>
+      <JobsTable />
     </>
   );
 }
