188d31790abf062807887ae571d5d200bbe44e00
Annie Cai <caishajia@gmail.com>
2023-07-30 23:35:47 -0400

    fixed mobile autodesk spacing

--- website-v3.0/src/components/WorkedAt.js
+++ website-v3.0/src/components/WorkedAt.js
@@ -27,20 +27,20 @@ export default function WorkedAt({ show }) {
       <h2>Our Community has Worked At</h2>
       <Container fluid className="my-5">
         <Row>
-          <Col xs={4} md={2} className="company-image"><img height={60} src={Salesforce} alt={"Sales Force"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={60} src={Meta} alt={"Meta"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={70} src={Microsoft} alt={"Microsoft"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={50} src={Apple} alt={"Apple"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={80} src={Riotgames} alt={"Riot Games"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={70} src={Ibm} alt={"IBM"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={60} src={Salesforce} alt={"Sales Force"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={60} src={Meta} alt={"Meta"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={70} src={Microsoft} alt={"Microsoft"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={50} src={Apple} alt={"Apple"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={80} src={Riotgames} alt={"Riot Games"} /></Col>
+          <Col xs={6} md={2} className="company-image"><img height={70} src={Ibm} alt={"IBM"} /></Col>
         </Row>
-        <Row className={"my-4"}>
-          <Col xs={4} md={2} className="company-image"><img height={60} src={Lyft} alt={"Lyft"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={30} src={Wish} alt={"Wish"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={30} src={Datadog} alt={"Datadog"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={50} src={AmericanExpress} alt={"AmericanExpress"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={30} src={Shopify} alt={"Shopify"} /></Col>
-          <Col xs={4} md={2} className="company-image"><img height={35} src={Autodesk} alt={"Autodesk"} /></Col>
+        <Row className={"my-3"}>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={40} src={Lyft} alt={"Lyft"} /></Col>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={30} src={Wish} alt={"Wish"} /></Col>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={30} src={Datadog} alt={"Datadog"} /></Col>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={50} src={AmericanExpress} alt={"AmericanExpress"} /></Col>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={30} src={Shopify} alt={"Shopify"} /></Col>
+          <Col xs={6} md={2} className="company-image pb-4"><img height={30} src={Autodesk} alt={"Autodesk"} /></Col>
         </Row>
         <div className="spacer"></div>
       </Container>
