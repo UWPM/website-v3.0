3c5726702799b5930b8e52bff707d6e4e8d2fa96
Simeng He <28720612+simenghe@users.noreply.github.com>
2023-10-18 18:32:35 -0400

    Add more images

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -7,15 +7,15 @@ export default function Events({ show }) {
   return (
     <>
       <div id="homeIntro">
-          <>
-            <div className="desktop-hex">
-              <img src={desktopHexagons} alt="" />
-            </div>
-            <div className="main-text-box">
-              <h1 className="main-text">Past events</h1>
-              <hr/>
-            </div>
-          </>
+        <>
+          <div className="desktop-hex">
+            <img src={desktopHexagons} alt="" />
+          </div>
+          <div className="main-text-box">
+            <h1 className="main-text">Past events</h1>
+            <hr />
+          </div>
+        </>
       </div>
     </>
   );
