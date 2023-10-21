e34442eb0762606a7f49c9d5ff9b5e951bebb887
navdeep03gill <navdeep03gill@gmail.com>
2023-10-21 18:55:20 -0400

    feature: About Title + Hexagons

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,15 +1,33 @@
 import React from "react";
 import desktopHexagons from "../images/background/desktop-hexagons.svg";
+import {Container, Row, Col} from "react-bootstrap";
 import "../styles/Events.css";
 import "../App.css";
+//import "../styles/Home.css"
 
 export default function Events({ show }) {
   return (
     <>
-      <div className="desktop-hex">
-        <img src={desktopHexagons} alt="" />
-      </div>
-      <div>This is the events page</div>
+      <Container>
+          <div className="events-container">
+            <div className="hex-container">
+              <img src={desktopHexagons} alt="" />
+            </div>
+            <div className="figCaption">
+              <h1 className="title-text">Gain product skills through our case competition, workshops, and more.</h1>
+            </div>
+          </div>
+      </Container>
+
+      <Container>
+        <div>
+          <h1>
+            
+          </h1>
+        </div>
+
+      </Container>
+
     </>
   );
 }
