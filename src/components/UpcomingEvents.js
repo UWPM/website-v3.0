2ba933acfeb01a42a0a35e6b75749ad92ed0fdc5
KrishnaJoshi116 <krishna.joshi1169@gmail.com>
2023-10-22 23:43:46 -0400

    Refactor Upcoming Events Page

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,39 +1,74 @@
-import React from 'react';
+import React from "react";
 import "../styles/Events.css";
+import placeholder from "../images/events/placeholder.png";
 
-const UpcomingEventsCard = ({ title, date, location, cto, about, description, subtitle, point1, point2, point3, point4, imagePath}) => {
+const UpcomingEventsCard = ({
+  title,
+  date,
+  location,
+  buttonMessage,
+  about,
+  description,
+  subtitle,
+  point1,
+  point2,
+  point3,
+  point4,
+}) => {
+  title = "Upcoming Event";
+  date = "November 18th 2023";
+  location = "Location TBD";
+  buttonMessage = "APPLY NOW";
+  about = "About";
+  description =
+    "ProdCon UW PM’s flagship event that typically happens every fall. ProdCon is a day-long product management case study competition, where students compete in teams of two to four to tackle an industry product challenge.";
+  subtitle = "Why Attend ProdCon?";
+  point1 = "Network with industry PM and like-minded students";
+  point2 = "Build experiential product skills";
+  point3 = "Solve real-world problems";
+  point4 = "Win Prizes";
+  const points = [point1, point2, point3, point4];
   return (
     <div className="container-full">
       <div className="title-section">
-      <h2>{title}</h2>
-        <hr/>
+        <h2>{title}</h2>
+        <hr />
       </div>
-    <div className="main-section">
-      <div>
-        <div className="section">
-        <div className="inner-section">
-        <h2 className> ProdCon</h2>
-        <p>{date} <br/> {location}</p>
+      <div className="main-section">
+        <div>
+          <div className="section">
+            <div className="inner-section">
+              <h2 className> ProdCon</h2>
+              <p>
+                {date} <br /> {location}
+              </p>
+            </div>
+            <a
+              href="https://www.instagram.com/p/CyWVHpNIC-i/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="red-button">{buttonMessage}</button>
+            </a>
+          </div>
+          <div className="inner-section">
+            <p className="about">{about}</p>
+            <p className="description">{description}</p>
+          </div>
+          <div className="inner-section">
+            <p className="sub-title">{subtitle}</p>
+            <ul>
+              {points.map((point, index) => (
+                <li key={index}>{point}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <button className = "red-button">{cto}</button>
-        </div>
-        <div className="inner-section">
-        <p className = "about">{about}</p>
-        <p className="description">{description}</p>
-        </div>
-        <div className="inner-section">
-        <p className = "sub-title" >{subtitle}</p>
-        <ul>
-            <li>{point1}</li>
-            <li>{point2}</li>
-            <li>{point3}</li>
-            <li>{point4}</li>
-        </ul>
-        </div>
+        <a href="https://www.instagram.com/p/CyWVHpNIC-i/?hl=en">
+          <img className="image" src={placeholder} alt="" />
+        </a>
       </div>
-      <img className= "image" src={imagePath} alt="" />
-      </div>
-      </div>
+    </div>
   );
 };
 
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -4,7 +4,6 @@ import "../styles/Events.css";
 import "../App.css";
 import UpcomingEventsCard from "../components/UpcomingEvents.js";
 import context from "react-bootstrap/esm/AccordionContext";
-import placeholder from '../images/events/placeholder.png';
 
 export default function Events({ show }) {
   return (
@@ -12,21 +11,7 @@ export default function Events({ show }) {
       <div className="desktop-hex">
         <img src={desktopHexagons} alt="" />
       </div>
-        <UpcomingEventsCard
-          title="Upcoming Event"
-          date="November 18th 2023" 
-          location="Location TBD"
-          cto="APPLY NOW"
-          about="About"
-          description="ProdCon UW PM’s flagship event that typically happens every fall. ProdCon is a day-long product management case study competition, where students compete in teams of two to four to tackle an industry product challenge."
-          subtitle="Why Attend ProdCon?"
-          point1="Network with industry PM and like-minded students"
-          point2="Build experiential product skills"
-          point3="Solve real-world problems"
-          point4="Win Prizes"
-          imagePath={placeholder}
-        />
+      <UpcomingEventsCard />
     </>
   );
 }
-
