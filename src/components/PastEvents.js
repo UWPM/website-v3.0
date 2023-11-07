f1c84c7c259d74e57c2eebb6f2bfafeb36ba7a19
Simeng He <28720612+simenghe@users.noreply.github.com>
2023-11-07 14:35:00 -0500

    more mobile friendly view with 2 columns showing

--- src/components/PastEvents.js
+++ src/components/PastEvents.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Events.css';
 import '../App.css';
 import { Col, Container, Image, Row } from 'react-bootstrap';
@@ -10,35 +10,56 @@ import win23IceCream from '../images/events/win23-ice-cream.png';
 import fall22Prodcon from '../images/events/fall22-prodcon2.jpeg';
 
 export default function PastEvents() {
-  // Support image rows of size 3 right now
-  const imageRows = [
-    [
-      [speakerSneakPeak, 'https://www.instagram.com/p/CuinX2tANsc/'],
-      [blueprintUWPM, 'https://www.instagram.com/p/CuVlp-SJFqk/'],
-      [win23ProductPanel, 'https://www.instagram.com/p/CqbWlGKgcaV/'],
-    ],
-    [
-      [win23IceCream, 'https://www.instagram.com/p/CpoA_B7gBpN/'],
-      [win23ResumeCritique, 'https://www.instagram.com/p/Co-ZyYNJR6D/'],
-      [fall22Prodcon, 'https://www.instagram.com/p/CjycXW0gcyo/'],
-    ],
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const flatImages = [
+    [speakerSneakPeak, 'https://www.instagram.com/p/CuinX2tANsc/'],
+    [blueprintUWPM, 'https://www.instagram.com/p/CuVlp-SJFqk/'],
+    [win23ProductPanel, 'https://www.instagram.com/p/CqbWlGKgcaV/'],
+    [win23IceCream, 'https://www.instagram.com/p/CpoA_B7gBpN/'],
+    [win23ResumeCritique, 'https://www.instagram.com/p/Co-ZyYNJR6D/'],
+    [fall22Prodcon, 'https://www.instagram.com/p/CjycXW0gcyo/'],
   ];
+
+  const getCols = (data) => {
+    const colsPerRow = isMobile ? 2 : 3;
+    const rows = [];
+
+    for (let i = 0; i < data.length; i += colsPerRow) {
+      const rowImages = data.slice(i, i + colsPerRow);
+
+      const colComponents = rowImages.map(([imageUrl, instagramUrl], index) => (
+        <Col key={index} xs={6} md={4} className="p-4">
+          <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
+            <Image src={imageUrl} alt={`Image ${index + 1}`} fluid />
+          </a>
+        </Col>
+      ));
+
+      rows.push(<Row key={i}>{colComponents}</Row>);
+    }
+
+    return rows;
+  };
+
   return (
     <Container>
       <h2>Past events</h2>
       <hr></hr>
-      <Container className="my-10">
-        {imageRows.map((x) => (
-          <Row>
-            {x.map(([imageSource, instagramURL]) => (
-              <Col className="p-4">
-                <a href={instagramURL}>
-                  <Image fluid src={imageSource} alt="" />
-                </a>
-              </Col>
-            ))}
-          </Row>
-        ))}
+      <Container className="my-10" fluid>
+        {getCols(flatImages)}
         <div className="spacer"></div>
       </Container>
     </Container>
