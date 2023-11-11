9b60c8a11553ed1a23a4e3e6147eceecfed6a0ad
KrishnaJoshi116 <krishna.joshi1169@gmail.com>
2023-11-11 00:13:23 -0500

    Fixed Responsiveness

--- src/pages/About.js
+++ src/pages/About.js
@@ -68,6 +68,68 @@ export default function About({ show }) {
           </Col>
         </Row>
       </Container>
+      <div>
+        <div class="values-title-wrapper">
+          <div class="title-div">
+            <h2>Values</h2>
+          </div>
+          <div class="grid-container">
+            <div class="grid-item">
+              <img src={innovationImg} class="icons-image" />
+              <div class="text-box">
+                <h3>Innovation</h3>
+                <p>
+                  We value innovation as the driving force behind successful
+                  products, that comes from encouraging creative thinking, the
+                  exploration of new ideas, and the pursuit of inventive
+                  solutions to real-world problems.
+                </p>
+              </div>
+            </div>
+            <div class="grid-item">
+              {' '}
+              <img src={empowermentImg} class="icons-image" />
+              <div class="text-box">
+                <h3>Empowerment</h3>
+                <p>
+                  We empower anyone to succeed and break into the world of
+                  product management by providing the resources, knowledge, and
+                  opportunities necessary to succeed. We believe in equipping
+                  individuals with the skills and experiences to work in
+                  product, no matter their background.
+                </p>
+              </div>
+            </div>
+            <div class="grid-item">
+              {' '}
+              <img src={learningImg} class="icons-image" />
+              <div class="text-box">
+                <h3>Continuous Learning</h3>
+                <p>
+                  We embrace a growth mindset and value continuous learning for
+                  all our members. Members are encouraged to expand their
+                  horizons on the world of product from learning from everyone
+                  around them to strive for personal and professional
+                  development.
+                </p>
+              </div>
+            </div>
+            <div class="grid-item">
+              {' '}
+              <img src={communityImg} class="icons-image" />
+              <div class="text-box">
+                <h3>Community Engagement</h3>
+                <p>
+                  We actively engage with the Waterloo product management
+                  community, both with alumni, current students and beyond, to
+                  exchange knowledge and build valuable long-lasting
+                  connections.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
