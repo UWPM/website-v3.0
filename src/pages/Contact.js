5cb0b8833280abb0b882e39467a435df0ff7f9f3
jenniferyingg <jenniferxying@gmail.com>
2023-11-14 19:01:54 -0500

    contact page styles

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import WorkedAt from '../components/WorkedAt';
+import '../styles/Contact.css';
+import '../App.css';
 
 export default function Contact({ show }) {
   return (
